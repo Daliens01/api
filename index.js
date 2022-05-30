8d7af8298ce381c8202483e951c919a6be83af34
Daliens01 <ortizbarahona2@gmail.com>
2022-05-30 13:18:23 -0500

    fixing stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const  mongoose  = require("utils/DBConection")
+const  mongoose  = require("./src/utils/DBConection")
 const app = express()
 const port = process.env.PORT || 4000
-const userRoutes = require("routes/options")
+const userRoutes = require("./src/routes/options")
 const cors = require("cors")
 app.use(cors())
 //middleware
